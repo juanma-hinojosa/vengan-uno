104523bf79d285b0053fbf4b21d99b46236f258f
Juan  Hinojosa <juanma.hinojosa97@gmail.com>
2023-08-10 18:34:33 -0300

    Add Panelistas

---
+++ src/components/SectionReverseComponent.jsx
@@ -0,0 +1,17 @@
+function SectionReverseComponent(props) {
+    return (
+        <figure className="container col-xxl-8 px-4 py-5 ">
+            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div className="col-10 col-sm-8 col-lg-6">
+                    <img src={props.foto} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+                </div>
+                <div className="col-lg-6 text-white">
+                    <h1 className="display-5 fw-bold lh-1 mb-3 orange">{props.nombre}</h1>
+                    <p className="light">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                </div>
+            </div>
+        </figure>
+    )
+}
+
+export default SectionReverseComponent
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,37 @@
 import SectionComponent from "../components/SectionComponent";
 import Bio from '../images/bio-h1.png'
+import Juan from '../images/bio-2.jpg'
+import Cuffaro from '../images/bio-3.jpg'
+import Alan from '../images/bio-4.jpg'
+import Medina from '../images/bio-5.jpg'
+import SectionReverseComponent from "../components/SectionReverseComponent";
 
 function About() {
-    return(
-        <main>
-            <SectionComponent 
-                nombre={'Nanuel Castro'}
+    return (
+        <main className="bg-dark">
+            <SectionComponent
+                nombre={'Nahuel Castro'}
                 foto={Bio}
+            />
+
+            <SectionReverseComponent
+                nombre={'Juan'}
+                foto={Juan}
+            />
+
+            <SectionComponent
+                nombre={'Cuffaro Mufaro'}
+                foto={Cuffaro}
+            />
+
+            <SectionReverseComponent
+                nombre={'Alan'}
+                foto={Alan}
+            />
+
+            <SectionComponent
+                nombre={'Federico Medina'}
+                foto={Medina}
             />
         </main>
     )
